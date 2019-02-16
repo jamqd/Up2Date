c2aacfd793db65a9dae33035041e81d8f4195fa8
Bill Liu <bill.liu.school@gmail.com>
2019-02-16 11:41:38 -0800

    url fix?

--- up2date/newsQuery/urls.py
+++ up2date/newsQuery/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('auth/', views.authenticate, name='authentication'),
     path('preferences/', views.pref_update, name='Preferences'),
     path('updated/', views.updated, name='Thank you')
+    url(r'^$', 'myview', name='myview')
 ]
