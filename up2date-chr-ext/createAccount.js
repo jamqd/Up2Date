87ae210033dc9b9e0e018e0c095c8b1fbbcd9547
Bill Liu <bill.liu.school@gmail.com>
2019-02-16 11:24:33 -0800

    pushed rest of repo

--- up2date-chr-ext/createAccount.js
+++ up2date-chr-ext/createAccount.js
@@ -4,7 +4,7 @@ function createAccount(json) {
     const Httpurl = "http://django-ev.2tuewqdzwb.us-west-1.elasticbeanstalk.com/auth/";
     Httpreq.open("POST", Httpurl, true);
 
-    Httpreq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    Httpreq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     Httpreq.onreadystatechange = function() {
         if (Httpreq.readyState == 4) {
             alert(Httpreq.responseText);
