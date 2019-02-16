f4dd2bd32981562fe978f86f64c2381d595435cf
Bill Liu <bill.liu.school@gmail.com>
2019-02-16 04:49:45 -0800

    updated requirements.txt

--- up2date/newsQuery/views.py
+++ up2date/newsQuery/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-dict = {} #save this somewhere later to persist through calls
 
-def query(search_term, from_date=1262304000, article_count=10, subscription_key="db529dd884ae4732a2bf1a453aa66bb1"): #use epoch time
+def query(search_term, from_date = 1262304000, article_count=100, subscription_key="db529dd884ae4732a2bf1a453aa66bb1"): #use epoch time
     search_url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search"
     headers = {"Ocp-Apim-Subscription-Key" : subscription_key}
     params  = {"count": article_count, "q": search_term, "since": from_date, "sortBy": "Date", "textDecorations": True, "textFormat": "HTML"}
     response = requests.get(search_url, headers=headers, params=params)
     response.raise_for_status()
     search_results = response.json()
+    dict_rank = {} #save this somewhere later to persist through calls
     info = [(article["name"], article["provider"][0]["name"], article["url"], article["datePublished"]) for article in search_results["value"]]
     for i in info:
-        if i[1] in dict.keys():
-            dict[i[1]] += 1
+        if i[1] in dict_rank.keys():
+            dict_rank[i[1]] += 1
         else :
-            dict[i[1]] = 1
+            dict_rank[i[1]] = 1
     print("info below:")
     print(info)
-    return info #returns (name, source name, url, datepublished)
+    return dict_rank #returns (name, source name, url, datepublished)
     
 # Create your views here.
 @csrf_exempt
