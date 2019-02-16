ddcc98394fd6e6a9428ef96b34274c99d0376920
John Dang <john.amq.dang@gmail.com>
2019-02-16 10:21:26 -0800

    fixed typo

--- authUser.py
+++ authUser.py
@@ -3,10 +3,14 @@ from firebase_admin import auth
 import database
 
 def addAuthUser(name, email, password):
-    uid = database.addUser(name, email, password)
+    uid = database.addUser(name, email)
     auth.create_user(uid=uid,display_name=name,email=email, password=password)
 
 def getUserId(email):
     userRecord = auth.get_user_by_email(email)
+    print(userRecord.user_metadata)
     return userRecord.uid
 
+addAuthUser("John", "john.amq.dang@gmail.com", "bobasdfas")
+
+getUserId("john.amq.dang@gmail.com")
