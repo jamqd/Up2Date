2159bf03193275b7dd3950550fa385a77eab6e75
John Dang <john.amq.dang@gmail.com>
2019-02-16 15:18:06 -0800

    added signin

--- up2date/newsQuery/authUser.py
+++ up2date/newsQuery/authUser.py
@@ -1,6 +1,9 @@
 import firebase_admin
 from firebase_admin import auth
-from . import database
+# from . import database
+import database
+import json
+import urllib
 
 def addAuthUser(name, email, password):
     uid = database.addUser(name, email)
@@ -10,3 +13,38 @@ def getUserId(email):
     userRecord = auth.get_user_by_email(email)
     print(userRecord.user_metadata)
     return userRecord.uid
+
+def signIn(email, password):
+    """
+    Returns None if password is wrong
+    Returns uid if logged in
+    """
+
+    my_data = dict()
+    my_data["email"] = email
+    my_data["password"] = password
+    my_data["returnSecureToken"] = True
+				
+    json_data = json.dumps(my_data).encode()
+    headers = {"Content-Type": "application/json"}		
+    request = urllib.request.Request("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key="+"AIzaSyCrOIfhAMyWsOeMdxc2Y_tIKdQ5Lo7YkJs",
+                                         data=json_data, headers=headers)
+  		
+
+    try:
+        loader = urllib.request.urlopen(request)
+    except urllib.error.URLError as e:
+        message = json.loads(e.read())
+        print(message["error"]["message"])
+        return None
+    else:
+        print(json.load(loader)['localId'])
+        return json.load(loader)['localId']
+
+# cred = firebase_admin.credentials.Certificate('up2date-d815e-firebase-adminsdk-bvvmg-40710c7694.json')
+# default_app = firebase_admin.initialize_app(cred, options={
+#     'databaseURL': 'https://up2date-d815e.firebaseio.com/'
+# })
+
+# addAuthUser("John", "john.amq.dang@gmail.com", "fuckfuck")
+# signIn("john.amq.dang@gmail.com", "didy")
