adf6ec305d94dc3aa611da25f92d623e7295beac
Dylan Tran <dylantran@berkeley.edu>
2019-02-16 16:34:07 -0800

    began ranking function

--- query.py
+++ query.py
@@ -1,8 +1,13 @@
 import requests
 
-def query (search_term, from_date, to_date):
+def queryNewsApi (search_term, from_date, to_date): #date format yyyy-mm-dd one month back at most
     a = "https://newsapi.org/v2/everything?q={0}&from={1}&to={2}&sortBy=popularity&apiKey=3c906a21fbeb43c7819511120fd8a1c2".format(search_term, from_date, to_date)
     response = requests.get(a).json()
-    print(response)
+    return response
 
-query("bitcoin", "2019-01-26", "2019-02-15") #date format yyyy-mm-dd one month back at most
+# merge with views.py later
+def getRatio (search_term, ):
+
+def getControl (search_term):
+
+def getWeek (search_term):
