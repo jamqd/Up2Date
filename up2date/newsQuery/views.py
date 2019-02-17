5381561bd5729e404c6eae0a32e16250b8bd154d
Bill Liu <bill.liu.school@gmail.com>
2019-02-16 21:48:42 -0800

    debugging

--- up2date/newsQuery/views.py
+++ up2date/newsQuery/views.py
@@ -45,6 +45,10 @@ def query(search_term, from_date = 1262304000, article_count=100, count=100000, 
     newNews = queryNewsApi(search_term, '2019-02-10', '2019-02-17')
     for url in newNews:
         for article in search_results["value"]:
+            if dict_rank[article['provider'][0]['name']] > 10:
+                print(article['url'])
+                print(url)
+                print(dict_rank[article['provider'][0]['name']])
             if article['url'] == url and dict_rank[article['provider'][0]['name']] > 10:
                 newsList.append(article['url'])
     print('news list below')
